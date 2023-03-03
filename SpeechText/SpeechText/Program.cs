ac0b08465716fd72bdd752354d34110f75d6dabe
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2023-03-03 08:32:03 -0300

    feat: project finished

--- SpeechText/SpeechText/Program.cs
+++ SpeechText/SpeechText/Program.cs
@@ -1,39 +1,56 @@
-﻿//
-// For more samples please visit https://github.com/Azure-Samples/cognitive-services-speech-sdk 
-// 
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
-// Creates an instance of a speech config with specified subscription key and service region.
-using Windows.Media.SpeechSynthesis;
-
-string subscriptionKey = "undefined";
-string subscriptionRegion = "undefined";
-
-var config = SpeechConfig.FromSubscription(subscriptionKey, subscriptionRegion);
-// Note: the voice setting will not overwrite the voice element in input SSML.
-config.SpeechSynthesisVoiceName = "pt-BR-DonatoNeural";
-
-string text = "Oi, esta é Donato.";
-
-// use the default speaker as audio output.
-using (var synthesizer = new SpeechSynthesizer(config))
+class Program
 {
-    using (var result = await synthesizer.SpeakTextAsync(text))
-    {
-        if (result.Reason == ResultReason.SynthesizingAudioCompleted)
-        {
-            Console.WriteLine($"Speech synthesized for text [{text}]");
-        }
-        else if (result.Reason == ResultReason.Canceled)
-        {
-            var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
-            Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+    // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    static string speechKey = Environment.GetEnvironmentVariable("SPEECH_KEY");
+    static string speechRegion = Environment.GetEnvironmentVariable("SPEECH_REGION");
 
-            if (cancellation.Reason == CancellationReason.Error)
-            {
-                Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
-                Console.WriteLine($"CANCELED: Did you update the subscription info?");
-            }
+    static void OutputSpeechSynthesisResult(SpeechSynthesisResult speechSynthesisResult, string text)
+    {
+        switch (speechSynthesisResult.Reason)
+        {
+            case ResultReason.SynthesizingAudioCompleted:
+                Console.WriteLine($"Speech synthesized for text: [{text}]");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
+            default:
+                break;
         }
+    }
+
+    async static Task Main(string[] args)
+    {
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+
+        // The language of the voice that speaks.
+        speechConfig.SpeechSynthesisVoiceName = "en-US-JennyNeural";
+
+        using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+        {
+            // Get text from the console and synthesize to the default speaker.
+            Console.WriteLine("Enter some text that you want to speak >");
+            string text = Console.ReadLine();
+
+            var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text);
+            OutputSpeechSynthesisResult(speechSynthesisResult, text);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
     }
 }
